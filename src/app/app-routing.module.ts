9cb4ec093001e463254f52661b920c2cf314b0ff
Eric Maes <ericmaes.github@gmail.com>
2022-11-17 15:04:39 +0100

    Redirection for dialog-password in dialog-connexion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CarteComponent } from './components/carte/carte.component';
 import { InscriptionComponent } from './components/inscription/inscription.component';
 import { HomeComponent } from './home/home.component';
+import { DialogPasswordComponent } from './components/dialog-password/dialog-password.component';
 
 const appRoute: Routes = [
   {path: '', redirectTo:'Home', pathMatch:'full' },
   {path: 'Home', component: HomeComponent},
   {path: 'Cartes', component: CarteComponent},
   {path: 'Inscription', component: InscriptionComponent},
+  {path: 'RegenPassword', component: DialogPasswordComponent},
 ]
 
 @NgModule({
