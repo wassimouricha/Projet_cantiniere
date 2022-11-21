042259ce86b879618ebda569b0effd6ae47378ce
Eric Maes <ericmaes.github@gmail.com>
2022-11-21 15:36:39 +0100

    Add redirection to user-account page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { CarteComponent } from './components/carte/carte.component';
 import { InscriptionComponent } from './components/inscription/inscription.component';
 import { HomeComponent } from './home/home.component';
 import { DialogPasswordComponent } from './components/dialog-password/dialog-password.component';
+import { UserAccountComponent } from './components/user-account/user-account.component';
 
 const appRoute: Routes = [
   {path: '', redirectTo:'Home', pathMatch:'full' },
@@ -11,6 +12,7 @@ const appRoute: Routes = [
   {path: 'Cartes', component: CarteComponent},
   {path: 'Inscription', component: InscriptionComponent},
   {path: 'RegenPassword', component: DialogPasswordComponent},
+  {path: 'UserAccount', component: UserAccountComponent},
 ]
 
 @NgModule({
