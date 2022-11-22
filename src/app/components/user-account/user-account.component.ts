bd1421fea0d77c26876b4dbd2781045c40f1860e
Eric Maes <ericmaes.github@gmail.com>
2022-11-22 10:38:29 +0100

    Add informations and redirection on user-account page

--- src/app/components/user-account/user-account.component.ts
+++ src/app/components/user-account/user-account.component.ts
@@ -9,6 +9,20 @@ export class UserAccountComponent implements OnInit {
 
   constructor() { }
 
+  openCart(){
+    const cartIcon = document.querySelector(".cart-icon");
+    const cart = document.querySelector(".cart");
+    const closeCart = document.querySelector(".close-cart");
+
+    cartIcon!.addEventListener('click' , () => {
+        cart!.classList.add('active');
+    })
+
+    closeCart!.addEventListener('click' , () => {
+        cart!.classList.remove('active');
+    })
+}
+
   ngOnInit(): void {
   }
 
